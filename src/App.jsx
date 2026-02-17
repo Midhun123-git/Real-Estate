c3e50f9171cd3eee39fdfad5fdeee4d1d489b951
Midhun <midhunmadhuuu@gmail.com>
2026-02-17 17:29:42 +0530

    Card 2 Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Route,Routes} from 'react-router-dom'
 import Header from './Components/Header'
 import Card1 from './Components/Card1'
+import Card2 from './Components/Card2'
 const App = () => {
   return (
    <Routes>
     <Route path="/Header" element={<Header/>}/>
     <Route path="/Card1" element={<Card1/>}/>
+    <Route path="/Card2" element={<Card2/>}/>
     </Routes>
   )
 }
