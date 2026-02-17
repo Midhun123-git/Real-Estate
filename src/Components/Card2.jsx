520f66fcfe09154d0d35140b0f2d769d8ce5a6f8
Midhun <midhunmadhuuu@gmail.com>
2026-02-17 20:21:11 +0530

    Componenets Completed

--- src/Components/Card2.jsx
+++ src/Components/Card2.jsx
@@ -1,16 +1,95 @@
 import React from 'react'
-import Card from "../assets/Card5.jpg"
+import img1 from "../assets/Card2/img1.jpg"
+import img2 from "../assets/Card2/img2.jpg"
+import img3 from "../assets/Card2/img3.jpg"
+import img4 from "../assets/Card2/img4.jpg"
+import img5 from "../assets/Card2/img5.jpg"
+import img6 from "../assets/Card2/img6.jpg"
+import img7 from "../assets/Card2/img7.jpg"
+import img8 from "../assets/Card2/img8.jpg"
 import { FaStar } from "react-icons/fa";
 import { IoShareSocialOutline } from "react-icons/io5";
 import { MdFavorite } from "react-icons/md";
 const Card2 = () => {
+    const rowdet=[
+    {
+  id:"1",
+  img:img1,
+  price:"₹50 Lac",
+  house:"3BHK House",
+  place:"Kakkand, Kochi",
+  review:"4.8 (73)"
+    },
+    {
+        id:"2",
+        img:img2,
+        price:"₹50 Lac",
+        house:"3BHK House",
+        place:"Kakkand, Kochi",
+        review:"4.8 (73)"
+          },
+          {
+            id:"3",
+            img:img3,
+            price:"₹50 Lac",
+            house:"3BHK House",
+            place:"Kakkand, Kochi",
+            review:"4.8 (73)"
+              },
+              {
+                id:"4",
+                img:img4,
+                price:"₹50 Lac",
+                house:"3BHK House",
+                place:"Kakkand, Kochi",
+                review:"4.8 (73)"
+                  },
+    ]
+    const coldet=[
+        {
+      id:"5",
+      img:img5,
+      price:"₹50 Lac",
+      house:"3BHK House",
+      place:"Kakkand, Kochi",
+      review:"4.8 (73)"
+        },
+        {
+            id:"6",
+            img:img6,
+            price:"₹50 Lac",
+            house:"3BHK House",
+            place:"Kakkand, Kochi",
+            review:"4.8 (73)"
+              },
+              {
+                id:"7",
+                img:img7,
+                price:"₹50 Lac",
+                house:"3BHK House",
+                place:"Kakkand, Kochi",
+                review:"4.8 (73)"
+                  },
+                  {
+                    id:"8",
+                    img:img8,
+                    price:"₹50 Lac",
+                    house:"3BHK House",
+                    place:"Kakkand, Kochi",
+                    review:"4.8 (73)"
+                      },
+    ]
   return (
+    <div className='flex flex-col gap-4  '>
+        
+   <div className='flex gap-3'>
+   {rowdet.map((items)=>(
     <div className='w-full lg:w-[392px] h-auto
     bg-[#FFFFFF] rounded-2xl  border flex flex-col gap-2  
     '>  
     {/* top Img  Starts */}
       <div className='w-full h-[246px] p-2 relative'>
-        <img src={Card} alt="" className='w-full h-full object-cover rounded-2xl'/>
+        <img src={items.img} alt="" className='w-full h-full object-cover rounded-2xl'/>
         {/* Share Icon Starts */}
         <div className='w-[34px] h-[34px] bg-[#FFFFFF]/60
         cursor-pointer
@@ -32,25 +111,78 @@ const Card2 = () => {
       <div className='flex flex-col gap-1'>
         {/* Price Section Starts*/}
           
-                <p className='pl-2  font-bold text-[22px]'>₹50 Lac</p>
-                <p className='pl-2  font-bold text-[22px]'>3BHK House</p>
+                <p className='pl-2  font-bold text-[22px]'>{items.price}</p>
+                <p className='pl-2  font-bold text-[22px]'>{items.house}</p>
                  
           </div>
            {/* Price Section Ends*/}
               {/* Place And  Review  Starts*/}
                <div  className='flex justify-between pb-2'>
-                  <p className='text-[#7D7F88] font-normal text-[18px] pl-2'> Kakkand, Kochi</p>
+                  <p className='text-[#7D7F88] font-normal text-[18px] pl-2'> {items.place}</p>
 
                   <div className='flex gap-1 items-center mr-2'>
                   <FaStar size={16} className='text-[#FFBF75]'/>
-                        <p>4.8 (73)</p>
+                        <p>{items.review}</p>
                   </div>
                </div>
               {/* Place And  Review  Endss*/}
                
       </div>
+))}
+        
+      
+</div>
+<div className='flex gap-3'>
+   {coldet.map((items)=>(
+    <div className='w-full lg:w-[392px] h-auto
+    bg-[#FFFFFF] rounded-2xl  border flex flex-col gap-2  
+    '>  
+    {/* top Img  Starts */}
+      <div className='w-full h-[246px] p-2 relative'>
+        <img src={items.img} alt="" className='w-full h-full object-cover rounded-2xl'/>
+        {/* Share Icon Starts */}
+        <div className='w-[34px] h-[34px] bg-[#FFFFFF]/60
+        cursor-pointer
+        rounded-full flex items-center justify-center absolute top-4 left-[75%]
+        '>
+              <IoShareSocialOutline size={16} className='text-[#11A6A1]'/>
+        </div>
+        {/* Share Icon Ends */}
+        {/* Favorite Icon Starts */}
+        <div className='w-[34px] h-[34px] bg-[#FFFFFF]/60
+        rounded-full flex items-center justify-center absolute top-4 left-[88%]
+        cursor-pointer
+        '>
+              <MdFavorite size={16} className='text-[#11A6A1]'/>
+        </div>
+        {/* Favorite Icon Ends */}
+      </div>
+       {/* top Img  Starts */}
+      <div className='flex flex-col gap-1'>
+        {/* Price Section Starts*/}
+          
+                <p className='pl-2  font-bold text-[22px]'>{items.price}</p>
+                <p className='pl-2  font-bold text-[22px]'>{items.house}</p>
+                 
+          </div>
+           {/* Price Section Ends*/}
+              {/* Place And  Review  Starts*/}
+               <div  className='flex justify-between pb-2'>
+                  <p className='text-[#7D7F88] font-normal text-[18px] pl-2'> {items.place}</p>
 
-    
+                  <div className='flex gap-1 items-center mr-2'>
+                  <FaStar size={16} className='text-[#FFBF75]'/>
+                        <p>{items.review}</p>
+                  </div>
+               </div>
+              {/* Place And  Review  Endss*/}
+               
+      </div>
+))}
+        
+      
+</div>
+    </div>
   )
 }
 
--- src/Components/Card3.jsx
+++ src/Components/Card3.jsx
@@ -1,56 +1,105 @@
 import React from 'react'
-import Card from "../assets/Card6.jpg"
+import img1 from "../assets/Card3/img1.jpg"
+import img2 from "../assets/Card3/img2.jpg"
+import img3 from "../assets/Card3/img3.jpg"
+import img4 from "../assets/Card3/img4.jpg"
 import { FaStar } from "react-icons/fa";
 import { IoShareSocialOutline } from "react-icons/io5";
 import { MdFavorite } from "react-icons/md";
 const Card3 = () => {
+    const cards=[
+        {
+            id:"1",
+            img:img1,
+            price:"₹10,000",
+            month:"/Month",
+            house:"3BHK House",
+            place:" Kakkand, Kochi",
+            review:" Kakkand, Kochi"
+        },
+        {
+            id:"1",
+            img:img2,
+            price:"₹10,000",
+            month:"/Month",
+            house:"3BHK House",
+            place:" Kakkand, Kochi",
+            review:" Kakkand, Kochi"
+        },
+        {
+            id:"1",
+            img:img3,
+            price:"₹10,000",
+            month:"/Month",
+            house:"3BHK House",
+            place:" Kakkand, Kochi",
+            review:" Kakkand, Kochi"
+        },
+        {
+            id:"1",
+            img:img4,
+            price:"₹10,000",
+            month:"/Month",
+            house:"3BHK House",
+            place:" Kakkand, Kochi",
+            review:" Kakkand, Kochi"
+        },
+    ]
   return (
-    <div className='w-full lg:w-[392px] h-auto
-    bg-[#FFFFFF] rounded-2xl  border flex flex-col gap-2  
-    '>  
-    {/* top Img  Starts */}
-      <div className='w-full h-[246px] p-2 relative'>
-        <img src={Card} alt="" className='w-full h-full object-cover rounded-2xl'/>
-        {/* Share Icon Starts */}
-        <div className='w-[34px] h-[34px] bg-[#FFFFFF]/60
-        cursor-pointer
-        rounded-full flex items-center justify-center absolute top-4 left-[75%]
-        '>
-              <IoShareSocialOutline size={16} className='text-[#11A6A1]'/>
-        </div>
-        {/* Share Icon Ends */}
-        {/* Favorite Icon Starts */}
-        <div className='w-[34px] h-[34px] bg-[#FFFFFF]/60
-        rounded-full flex items-center justify-center absolute top-4 left-[88%]
-        cursor-pointer
-        '>
-              <MdFavorite size={16} className='text-[#11A6A1]'/>
-        </div>
-        {/* Favorite Icon Ends */}
-      </div>
-       {/* top Img  Starts */}
-      <div className='flex flex-col gap-1'>
-        {/* Price Section Starts*/}
-          
-        <p className='p-2 pb-0 font-bold text-[22px]'>₹10,000
-                  <span className='text-[#7D7F88] font-normal text-[20px]'>/Month</span>
-                  </p>
-                <p className='pl-2  font-bold text-[22px]'>3BHK House</p>
-                 
-          </div>
-           {/* Price Section Ends*/}
-              {/* Place And  Review  Starts*/}
-               <div  className='flex justify-between pb-2'>
-                  <p className='text-[#7D7F88] font-normal text-[18px] pl-2'> Kakkand, Kochi</p>
 
-                  <div className='flex gap-1 items-center mr-2'>
-                  <FaStar size={16} className='text-[#FFBF75]'/>
-                        <p>4.8 (73)</p>
+    <div className='flex gap-2'>
+        {
+            cards.map((items)=>(
+                <div className='w-full lg:w-[392px] h-auto
+                bg-[#FFFFFF] rounded-2xl  border flex flex-col gap-2  
+                '>  
+                {/* top Img  Starts */}
+                  <div className='w-full h-[246px] p-2 relative'>
+                    <img src={items.img} alt="" className='w-full h-full object-cover rounded-2xl'/>
+                    {/* Share Icon Starts */}
+                    <div className='w-[34px] h-[34px] bg-[#FFFFFF]/60
+                    cursor-pointer
+                    rounded-full flex items-center justify-center absolute top-4 left-[75%]
+                    '>
+                          <IoShareSocialOutline size={16} className='text-[#11A6A1]'/>
+                    </div>
+                    {/* Share Icon Ends */}
+                    {/* Favorite Icon Starts */}
+                    <div className='w-[34px] h-[34px] bg-[#FFFFFF]/60
+                    rounded-full flex items-center justify-center absolute top-4 left-[88%]
+                    cursor-pointer
+                    '>
+                          <MdFavorite size={16} className='text-[#11A6A1]'/>
+                    </div>
+                    {/* Favorite Icon Ends */}
                   </div>
-               </div>
-              {/* Place And  Review  Endss*/}
-               
-      </div>
+                   {/* top Img  Starts */}
+                  <div className='flex flex-col gap-1'>
+                    {/* Price Section Starts*/}
+                      
+                    <p className='p-2 pb-0 font-bold text-[22px]'>{items.price}
+                              <span className='text-[#7D7F88] font-normal text-[20px]'>{items.month}</span>
+                              </p>
+                            <p className='pl-2  font-bold text-[22px]'>{items.house}</p>
+                             
+                      </div>
+                       {/* Price Section Ends*/}
+                          {/* Place And  Review  Starts*/}
+                           <div  className='flex justify-between pb-2'>
+                              <p className='text-[#7D7F88] font-normal text-[18px] pl-2'>{items.place}</p>
+            
+                              <div className='flex gap-1 items-center mr-2'>
+                              <FaStar size={16} className='text-[#FFBF75]'/>
+                                    <p>{items.review}</p>
+                              </div>
+                           </div>
+                          {/* Place And  Review  Endss*/}
+                           
+                  </div>
+            ))
+        }
+ 
+    </div>
   )
 }
 
